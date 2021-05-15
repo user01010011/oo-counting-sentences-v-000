a2193e88f0f31842433da156e02f16186cd27fc0
Shasha Yan <shashayan@gmail.com>
2021-05-15 20:24:51 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split.(/\.|\?|\!/).delete_if {|w| w.count < 2}.count
+    self.split(/\.|\?|\!/).delete_if {|w| w.count < 1}.count
     if count == 0
       zero
     end
